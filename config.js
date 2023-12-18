e14f7a7205e2faee794c1cfe4b43dd09736c2cb5
candyrabbit2 <154230786+candyrabbit2@users.noreply.github.com>
2023-12-18 13:21:28 +0000

    Update 8 files

---
+++ config.js
@@ -0,0 +1,63 @@
+/*
+ * https://rentry.org/teralomaniac_clewd
+ * https://github.com/teralomaniac/clewd
+ */
+
+// SET YOUR COOKIE BELOW
+
+module.exports = {
+  Cookie: "SET YOUR COOKIE HERE",
+  CookieArray: ["sessionKey=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","sessionKey=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","sessionKey=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"]
+  Cookiecounter: 1,
+  CookieIndex: 0,
+  ProxyPassword: "123",
+  Ip: "127.0.0.1",
+  Port: 8444,
+  localtunnel: false,
+  BufferSize: 1,
+  SystemInterval: 3,
+  rProxy: "",
+  api_rProxy: "",
+  padtxt_placeholder: "",
+  PromptExperimentFirst: "",
+  PromptExperimentNext: "",
+  PersonalityFormat: "{{char}}'s personality: {{personality}}",
+  ScenarioFormat: "Dialogue scenario: {{scenario}}",
+  Settings: {
+    RenewAlways: true,
+    RetryRegenerate: false,
+    PromptExperiments: true,
+    SystemExperiments: true,
+    PreventImperson: false,
+    AllSamples: false,
+    NoSamples: false,
+    StripAssistant: false,
+    StripHuman: false,
+    PassParams: false,
+    ClearFlags: true,
+    PreserveChats: false,
+    LogMessages: true,
+    FullColon: true,
+    padtxt: 15000,
+    xmlPlot: true,
+    Superfetch: true,
+  },
+};
+
+/*
+ BufferSize
+ * How many characters will be buffered before the AI types once
+ * lower = less chance of `PreventImperson` working properly
+
+ ---
+
+ SystemInterval
+ * How many messages until `SystemExperiments alternates`
+
+ ---
+
+ Other settings
+ * https://gitgud.io/ahsk/clewd/#defaults
+ * and
+ * https://gitgud.io/ahsk/clewd/-/blob/master/CHANGELOG.md
+ */
